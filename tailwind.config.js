dad8c4da5411c89dae16e42aaccb6f253ddba20f
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-09-18 22:05:13 +0700

    feat(setup): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'c-01': '#FFB640',
+        'c-02': '#FF9D00',
+        'c-03': '#FF6254',
+        'c-04': '#1BD5E1',
+        'c-05': '#129AA3',
+      },
+    },
   },
   plugins: [],
 };
