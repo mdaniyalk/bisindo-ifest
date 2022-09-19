49a14e4f42a98caf890c96f625c3ef9eee231c28
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-09-19 08:00:46 +0700

    feat(recognitor): camera and camera switcher

---
+++ components/elements/camera/Camera.js
@@ -0,0 +1,25 @@
+import Webcam from 'react-webcam';
+
+// TODO: receive data stream
+export function Camera({ selectedDeviceId }) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Recording_a_media_element
+  const userMediaHandler = (data) => {
+    console.log(data);
+    const track = data.getVideoTracks()[0];
+    console.log(track);
+    track.oncapturehandlechange = () => console.log('p');
+  };
+
+  return (
+    <>
+      <div className="w-full">
+        <Webcam
+          audio={false}
+          videoConstraints={{ deviceId: selectedDeviceId }}
+          className="w-full"
+          onUserMedia={userMediaHandler}
+        />
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Camera, CameraSwitcher } from '/components/elements/camera';
 import DefaultLayout from '/components/layouts/DefaultLayout';
 
 export default function Home() {
+  const [selectedDeviceId, setSelectedDeviceId] = useState();
+  const [devices, setDevices] = useState([]);
+
+  const handleDevices = useCallback(
+    (mediaDevices) => {
+      setDevices(mediaDevices.filter(({ kind }) => kind === 'videoinput'));
+      if (devices?.length && !selectedDeviceId)
+        setSelectedDeviceId(devices[0]?.deviceId);
+    },
+    [devices, selectedDeviceId]
+  );
+
+  useEffect(() => {
+    navigator.mediaDevices?.enumerateDevices().then(handleDevices);
+  }, [handleDevices]);
+
   return (
     <DefaultLayout>
-      <div className="h-[50vh] bg-grad-green text-c-02">halo gan</div>
+      <div className="container mx-auto bg-blue-200">
+        {/* TODO: create camera selector and swithcer -> selector for web | switcher for mobile */}
+        <CameraSwitcher
+          devices={devices}
+          selectedDeviceId={selectedDeviceId}
+          setSelectedDeviceId={setSelectedDeviceId}
+        />
+        <Camera selectedDeviceId={selectedDeviceId} />
+      </div>
     </DefaultLayout>
   );
 }
