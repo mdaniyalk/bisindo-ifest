d7d27a64e3407504f4ee9e55a37274bd1d096652
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-10-03 20:29:43 +0700

    feat(dictionary): base responsive layout

--- pages/dictionary/index.js
+++ pages/dictionary/index.js
@@ -4,20 +4,8 @@ import { useMemo, useState } from "react";
 import { useRouter } from "next/router";
 import clsx from "clsx";
 import { Video } from "/components/elements/video";
-
-// const CATEGORIES = [
-//   "alphabet",
-//   "year",
-//   "month",
-//   "day",
-//   "transportation",
-//   "manner",
-//   "family",
-//   "city",
-//   "sport",
-//   "job",
-//   "place",
-// ];
+import { RenderIf } from "/components/elements/RenderIf";
+import { BackgroundDecoration } from "/components/elements/decoration/BackgroundDecoration";
 
 const CATEGORIES = {
   semua: "semua",
@@ -75,13 +63,15 @@ function Dictionary() {
   const [dictItems, setDictItems] = useState(DICT_ITEMS);
   const [search, setSearch] = useState("");
 
+  const [playedLabel, setPlayedLabel] = useState("desember");
+
   const filteredDictItems = useMemo(() => {
     const filteredByCategory = dictItems.filter((item) => {
       if (!category) return true;
       return item.category === category;
     });
     const filteredBySearch = filteredByCategory.filter((item) =>
-      item.label.includes(search),
+      item.label.includes(search.toLowerCase()),
     );
     return filteredBySearch;
   }, [category, dictItems, search]);
@@ -92,52 +82,74 @@ function Dictionary() {
   }, [category]);
 
   return (
-    <DefaultLayout title="SIBI Dictionary - Tutur">
-      <div className="flex w-full min-h-screen">
+    <DefaultLayout title="Kamus SIBI - Tutur">
+      <div className="flex w-full relative">
         {/* sidebar */}
-        <nav className="bg-yellow-100 h-screen shadow-xl rounded-tr-[3em] rounded-br-[3em] p-8 space-y-1 sticky top-0">
-          {Object.keys(CATEGORIES).map((category) => (
-            <div
-              key={category}
-              role="button"
-              onClick={() =>
-                router.push(
-                  category === CATEGORIES.semua
-                    ? "/dictionary"
-                    : `/dictionary?category=${category}`,
-                )
-              }
-              className={clsx(
-                "px-6 py-2 rounded-full font-medium text-white w-fit hover:-translate-y-[2px] hover:scale-105 duration-200",
-                activeCategory === category
-                  ? "bg-grad-green"
-                  : "bg-grad-orange",
-              )}
-            >
-              <span className="drop-shadow-md capitalize">{category}</span>
-            </div>
-          ))}
-        </nav>
+        <div className="py-4 h-[calc(100vh-56px)] sticky top-0 hidden md:block">
+          <nav className="bg-white backdrop-blur bg-opacity-10 shadow-2xl rounded-tr-[3em] rounded-br-[3em] p-8 space-y-1">
+            {Object.keys(CATEGORIES).map((category) => (
+              <div
+                key={category}
+                role="button"
+                onClick={() =>
+                  router.push(
+                    category === CATEGORIES.semua
+                      ? "/dictionary"
+                      : `/dictionary?category=${category}`,
+                  )
+                }
+                className={clsx(
+                  "px-6 py-2 rounded-full font-medium text-white w-fit hover:-translate-y-[2px] hover:scale-105 duration-200",
+                  activeCategory === category
+                    ? "bg-grad-green"
+                    : "bg-grad-orange",
+                )}
+              >
+                <span className="drop-shadow-md capitalize">{category}</span>
+              </div>
+            ))}
+          </nav>
+        </div>
 
         {/* content */}
-        <div className="p-8 pt-4 w-full">
-          {/* topbar */}
-          <div className="flex justify-between items-baseline sticky top-0 z-50 bg-white py-4 -mx-8 px-8">
-            <h1 className="text-2xl font-medium text-c-05">Kamus SIBI</h1>
-            <div className="group w-full md:w-[300px] flex items-center rounded-full border border-gray-300 shadow-sm  focus:border-indigo-300 px-4 text-c-05">
-              <HiSearch className="w-6 h-6 " />
-              <input
-                type="text"
-                className="block w-full border-0 focus:ring-0"
-                placeholder="search..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
+        <div className="p-4 md:p-8 md:pt-4 w-full h-[calc(100vh-56px)] flex flex-col">
+          <div className="">
+            {/* highlight */}
+            <div className="md:hidden">
+              <Video
+                src={`https://bisindo-surakarta.com/uploads/video/kosakata/video/${playedLabel}.mp4`}
+                label={playedLabel}
+                labelInside
               />
+            </div>
+
+            {/* topbar */}
+            <div className="flex justify-end items-baseline sticky top-8 z-50 mt-4 md:mt-0 drop-shadow-xl">
+              {/* <h1 className="text-2xl font-medium text-c-05">Kamus SIBI</h1> */}
+              <div className="group w-full md:w-[300px] flex items-center rounded-full border border-gray-300 shadow-sm  focus:border-indigo-300 px-4 text-c-05 bg-white">
+                <HiSearch className="w-6 h-6 " />
+                <input
+                  type="text"
+                  className="block w-full border-0 focus:ring-0"
+                  placeholder="cari..."
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </div>
             </div>
           </div>
 
           {/* video boxes */}
-          <div className="grid gap-4 grid-cols-2 lg:grid-cols-4 mt-8">
+          <RenderIf when={filteredDictItems.length === 0}>
+            {/* TODO: styling not found */}
+            <div className="text-center mt-4">
+              Tidak dapat menemukan
+              <div>
+                <strong>{search}</strong>
+              </div>
+            </div>
+          </RenderIf>
+          <div className="grid gap-2 md:gap-4 grid-cols-2 lg:grid-cols-4 mt-4 overflow-auto flex-1">
             {filteredDictItems.map((item, index) => (
               <Video
                 key={item.label}
@@ -147,6 +159,9 @@ function Dictionary() {
             ))}
           </div>
         </div>
+
+        {/* decoration */}
+        <BackgroundDecoration />
       </div>
     </DefaultLayout>
   );
