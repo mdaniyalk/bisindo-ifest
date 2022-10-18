d1b7c4b99ab613e04a5e1505d25c6c0eee34e7b7
Haikal Hilmi <haikal6255hilmi@gmail.com>
2022-10-18 12:21:24 +0700

    feat : navbar

---
+++ pages/translate/index.js
@@ -0,0 +1,10 @@
+import DefaultLayout from "/components/layouts/DefaultLayout";
+const Translate = () => {
+  return (
+    <DefaultLayout title="Translate SIBI - Tutur">
+      <div className="text-2xl">TRANSLATE</div>
+    </DefaultLayout>
+  );
+};
+
+export default Translate;
