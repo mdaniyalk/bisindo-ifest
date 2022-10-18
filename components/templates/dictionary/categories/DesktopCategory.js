7be00277c4bfd456968d3bd44846d4f988e33a15
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-10-18 13:21:16 +0700

    feat(dictionary): add new vocabularies

--- components/templates/dictionary/categories/DesktopCategory.js
+++ components/templates/dictionary/categories/DesktopCategory.js
@@ -4,12 +4,13 @@ import { BubbleButton } from "/components/elements/button";
 export function DesktopCategory({ categories, activeCategory }) {
   const router = useRouter();
   return (
-    <div className="py-4 h-[calc(100vh-56px)] sticky top-0 hidden md:block">
+    <div className="py-4 h-screen-no-header sticky top-0 hidden md:block">
       <nav className="bg-white backdrop-blur bg-opacity-10 shadow-2xl rounded-tr-[3em] rounded-br-[3em] p-8 space-y-1">
         {Object.keys(categories).map((category) => (
           <BubbleButton
             key={category}
-            text={category}
+            text={categories[category].split("_")}
+            className="w-max"
             onClick={() =>
               router.push(
                 category === categories.semua
